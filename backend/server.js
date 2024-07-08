30d1c48f31b75acf9a2b09b5694534fc6f36bbbd
gulshan07dev <gulshanku9135@gmail.com>
2024-07-08 23:45:14 +0530

    fix: db config and user model

--- backend/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app.js');
-const PORT = process.env.PORT || 600;
-
-app.listen(PORT, () => {
-    console.log(`server started at http://localhost:${PORT}`);
-})
--- backend/src/config/db.confg.ts
+++ backend/src/config/db.confg.ts
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const connectToDb = async () => {
-    mongoose.connect(process.env.MONGO_URI)
+    mongoose.connect(process.env.MONGO_URI!)
     .then((conn) => {console.log(`db connected: ${conn.connection.host}`);})
     .catch((e) => {console.log(`error in connected to db ${e.message}`);})
 }
 
-module.exports = connectToDb;
+export default connectToDb;
