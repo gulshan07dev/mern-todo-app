60ac4e452a16b9421dafa955f76f2d3cea653cfb
Gulshan kumar <gulshanku9135@gmail.com>
2024-07-18 15:45:44 +0530

    fix dashboard layout

--- client/src/layouts/DashboardLayout.tsx
+++ client/src/layouts/DashboardLayout.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import { Outlet } from "react-router-dom";
 
-import Navbar from "./Navbar";
+import Navbar from "./navbar/Navbar";
 import Sidebar from "./Sidebar";
 
 const DashboardLayout: FC = () => {
