9e6797b42a389ff5e30af2a7f0632b5ecd96cd56
Gulshan kumar <gulshanku9135@gmail.com>
2024-07-18 15:41:58 +0530

    fix navbar

--- client/src/layouts/navbar/Navbar.tsx
+++ client/src/layouts/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
-import { NavLink } from "react-router-dom";
-import { LucideTimer, SearchIcon } from "lucide-react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { LucideTimer, SearchIcon, User } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -11,8 +11,11 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import LogoutButton from "./LogoutButton";
+import useAuthStore from "@/app/authStore";
 
 export default function Navbar() {
+  const navigate = useNavigate();
+  const { isLoggedIn, user } = useAuthStore();
   return (
     <header className="sticky top-0 flex h-16 bg-background items-center justify-between border-b px-4 sm:px-6">
       <NavLink to="/" className="flex items-center gap-2">
@@ -26,25 +29,20 @@ export default function Navbar() {
         </Button>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant="ghost" size="icon" className="rounded-full">
-              <img
-                src="/placeholder.svg"
-                width="32"
-                height="32"
-                className="rounded-full"
-                alt="Avatar"
-              />
+            <Button variant="ghost" size="icon" className="rounded-full overflow-hidden">
+              {isLoggedIn ? <span className="bg-secondary size-full flex justify-center items-center text-primary text-lg font-roboto">{user?.name?.split("")[0]}</span> : <User />}
               <span className="sr-only">Toggle user menu</span>
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => navigate("/me")}>
+              Profile
+            </DropdownMenuItem>
             <DropdownMenuItem>Support</DropdownMenuItem>
             <DropdownMenuSeparator />
-            <LogoutButton />
+            {isLoggedIn && <LogoutButton />}
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
